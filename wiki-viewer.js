21afaf06f3ffc700cb49f365ea554a9fba66f59c
zdflower <zdflower@users.noreply.github.com>
2017-05-02 18:35:53 -0300

    Versión básica, parece que funciona.

--- wiki-viewer.js
+++ wiki-viewer.js
@@ -1,40 +1,39 @@
 /*
 acá tiene que ir código para que al clickear la lupa se reemplace por un formulario donde ingresar un texto que va a estar conectado con la api de wikipedia
 */
-
-/*
-
-cuando se cliquee la lupa (y también cuando se presione la tecla enter),
-	se debe leer lo que el usuario escribió en la caja de texto (aBuscar)
-	agregar eso a un acceso a la api de wikipedia: ".../api.php?action=query&format=json&titles=" + aBuscar
-	eso te va a devolver datos en formato json,
-	por ejemplo:
-	{
-		"batchcomplete": "",
-		"query": {
-			"normalized": ... ,
-			"pages": {
-				
-			}
+$('#buscadorInput').on('keyup', function buscar(event){
+		var aBuscar = $('#buscadorInput').val();
+		console.log("Quiere buscar: " + aBuscar);
+		$(this).css("background-color", event.which === 13 ? "#2E2D88" : '#fff');
+		$(this).css("color", event.which === 13 ? '#fff': "#2E2D88");
+		if (event.which === 13){
+			console.log("apretó enter");
+			$('#resultados').html("<p> Resultados obtenidos: " + aBuscar + "</p>");
+			//$('#resultados').css("background-color", "#2E2D88");
+			//$('#resultados').css("color", "white");
+			//alert("enter");
+		} else {
+			console.log("no apretó enter");
 		}
-	}
-
-*/
-
+});
 $('#lupa').on('click', function buscar(){
 	var aBuscar = $('#buscadorInput').val();
 	console.log("Quiere buscar: " + aBuscar);
-
+	var cantidadDeResultados = 10;
 	$.getJSON("https://es.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + aBuscar + "&origin=*", function(data){
 		//mostrar los resultados
-		//$('#resultados').html("Resultados obtenidos ...");
 		console.log(data);
+		$('#resultados').html("<h2> Resultados obtenidos: </h2>");
+		for (var i = 0; i < cantidadDeResultados; i++){
+			var titulo = data.query.search[i].title; //tengo que partirlo por los espacios y unir el resultado con _
+			$('#resultados').append("<h3><a href='https://es.wikipedia.org/wiki/" + titulo.split(" ").join("_") + "'>" + titulo + "</a></h3>");
+			$('#resultados').append("<p class='lead'>"+ data.query.search[i].snippet + " ... </p>");	
+		}
+		$('#resultados').css("background-color", "#2E2D88");
+		$('#resultados').css("color", "white");
 	});
-
-
-	$('#resultados').append("<p> Resultados obtenidos: " + aBuscar + "</p>");
-//	$('#resultados').append(aBuscar + "</p>");
-//	$('#resultados').append("<p></p>");
-	$('#resultados').css("background-color", "#2E2D88");
-	$('#resultados').css("color", "white");
 });
+
+
+
+
